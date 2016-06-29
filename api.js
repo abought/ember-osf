6b7ec93d92c580c7ffc1d23f5b84aa1f545d6e4d
Andy Boughton <abought@gmail.com>
2016-06-29 11:33:19 -0400

    Initial commit: a gh-pages branch
    Intended to serve built YUIDoc assets. This is a new/orphan branch for the repo.
    
    [#EOSF-81]
    [ci skip]

---
+++ api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "EmberOSF.InfinityCustomMixin",
+        "file-manager"
+    ],
+    "modules": [
+        "ember-osf_mixins_infinity-custom"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-osf/mixins/infinity-custom",
+            "name": "ember-osf_mixins_infinity-custom",
+            "description": "A custom overlay on ember-infinity that supports loading infinite and paginated relationships\nFor the most part, the API and semantics are identical to ember infinity, except that the means of configuring the store find method is more flexible\n (supporting relationship queries that do not operate via store methods)"
+        }
+    ],
+    "elements": []
+} };
+});
